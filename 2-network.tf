df9b8249c1c6c5513a1eaf8bd31c0c0ad8e4a306
Pwd9000-ML <pwd9000@hotmail.co.uk>
2024-04-15 12:58:30 +0100

    add dns zones for private endpoints

--- 2-network.tf
+++ 2-network.tf
@@ -22,3 +22,22 @@ module "vnet" {
   subnets                       = local.subnets
   virtual_network_address_space = ["10.4.0.0/24"]
 }
+
+# Create Private DNS Zone for privatelink - Storage and keyvault
+module "private_dns_zones" {
+  for_each = toset("privatelink.blob.core.windows.net", "privatelink.vault.azure.net")
+
+  source                = "Azure/avm-res-network-privatednszone/azurerm"
+  version = "0.1.1"
+
+  enable_telemetry      = true
+  resource_group_name   = azurerm_resource_group.rg.name
+  domain_name           = each.key
+  virtual_network_links = {
+    vnetlinks = {
+      vnetlinkname     = "vnetlink-${each.key}"
+      vnetid           = module.vnet.virtual_network_id
+      autoregistration = true
+      tags = {}
+    }
+}
