2960396c364ed404ae617c97cc30141d2a0479ad
Pwd9000-ML <pwd9000@hotmail.co.uk>
2024-04-15 15:22:09 +0100

    up

--- 2-network.tf
+++ 2-network.tf
@@ -24,21 +24,36 @@ module "vnet" {
 }
 
 # Create Private DNS Zone for privatelink - Storage and keyvault (Add more to "For_each" if needed)
-module "private_dns_zones" {
+resource "azurerm_private_dns_zone" "privatelink" {
   for_each = toset(["privatelink.blob.core.windows.net", "privatelink.vault.azure.net"])
-
-  source  = "Azure/avm-res-network-privatednszone/azurerm"
-  version = "0.1.1"
-
-  enable_telemetry    = true
+  name                = each.key
   resource_group_name = azurerm_resource_group.rg.name
-  domain_name         = each.key
-  virtual_network_links = {
-    vnetlinks = {
-      vnetlinkname     = "vnetlink-${each.key}"
-      vnetid           = module.vnet.virtual_network_id
-      autoregistration = false
-      tags             = {}
-    }
-  }
 }
+
+resource "azurerm_private_dns_zone_virtual_network_link" "privatelink" {
+  for_each = toset(["privatelink.blob.core.windows.net", "privatelink.vault.azure.net"])
+  name                = "vnetlink-${each.key}"
+  resource_group_name = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.privatelink[each.key].name
+  virtual_network_id  = module.vnet.virtual_network_id
+  registration_enabled = false
+}
+
+# module "private_dns_zones" {
+#   for_each = toset(["privatelink.blob.core.windows.net", "privatelink.vault.azure.net"])
+
+#   source  = "Azure/avm-res-network-privatednszone/azurerm"
+#   version = "0.1.1"
+
+#   enable_telemetry    = true
+#   resource_group_name = azurerm_resource_group.rg.name
+#   domain_name         = each.key
+#   virtual_network_links = {
+#     vnetlinks = {
+#       vnetlinkname     = "vnetlink-${each.key}"
+#       vnetid           = module.vnet.virtual_network_id
+#       autoregistration = false
+#       tags             = {}
+#     }
+#   }
+# }
